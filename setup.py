6a1c2214c217f286a85c8baf1f64c2ffc306216c
Harshita Bharadwaj <85123203+HarshitaMBharadwaj@users.noreply.github.com>
2024-10-15 16:46:04 -0400

    setup code updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chat Bot',
+    version= '0.0.0',
+    author= 'Harshita Madhukar Bharadwaj',
+    author_email= 'bharadwajharshita2000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
